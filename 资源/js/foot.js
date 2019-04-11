98539aa886154773373c107ab4b977b6ae3e7a2f
Administrator <1442188146@qq.com>
2019-04-12 00:16:34 +0800

    测试

---
+++ 资源/js/foot.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2019/4/9.
+ */
+document.writeln("<div class=\'container\'>");
+document.writeln("    <div class=\'row\'>");
+document.writeln("        <div class=\'col-md-12 text-center\'>Copyright @2019 www.tecode.com 码教授</div>");
+document.writeln("    </div>");
+document.writeln("</div>");
